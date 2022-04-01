2e8335f3d41aeeda658b315b3e1a776e457a4d3f
tjvsx <vanslotj@gmail.com>
2022-03-31 20:03:49 -0400

    cleanup

--- contracts/facets/Governance.sol
+++ contracts/facets/Governance.sol
@@ -5,8 +5,6 @@ pragma solidity ^0.8.0;
 import { ERC20TokenStorage } from '../storage/ERC20TokenStorage.sol';
 import { GovernanceStorage } from '../storage/GovernanceStorage.sol'; 
 
-
-
 contract Governance {
     
     event Propose(address _proposer, address _proposalContract, uint _endTime);
